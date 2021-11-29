73f23681fcb8fd51fe47bfc3301fc003aa2cba4f
Jason Fukura <jason@jasonfukura.com>
2021-11-29 15:31:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ class Modal {
         modal.setAttribute( 'aria-labelledby', 'modal-title' )
         modal.setAttribute( 'role', 'dialog' )
         modal.setAttribute( 'aria-modal', 'true' )
-        console.log(this.data.data.name)
+
         let html = `
   <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
     <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" style="" aria-hidden="true"></div>
